5c47648175b7d4290fa0e2d1d8f40362fc7e4a66
Douglas Trindade <douglastrindade22@hotmail.com>
2024-07-21 19:31:20 -0300

    feat: added new lib icons

--- src/app/auth/login-adm/page.tsx
+++ src/app/auth/login-adm/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Login } from "./components";
-import { MdKeyboardArrowLeft } from "react-icons/md";
+import { ArrowLeft01Icon } from "hugeicons-react";
 
 const LoginPage = () => {
   return (
     <div className="container mx-auto mt-6 mb-10 px-20">
-      <Button className="outline outline-1 outline-offset-1 outline-gray-400 rounded-full">
-        <MdKeyboardArrowLeft />
+      <Button className="outline outline-1 outline-gray-400 rounded-full">
+        <ArrowLeft01Icon size={24} />
         <span className="ml-3 font-bold text-xl">Voltar</span>
       </Button>
 
