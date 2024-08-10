31782e18e14085035080e0ded231c436cdf29eef
arielchama <arielchama45@gmail.com>
2024-08-10 14:10:23 +0100

    feat: Configured the user list

--- src/app/colaborador/page.tsx
+++ src/app/colaborador/page.tsx
@@ -1,21 +1,49 @@
 "use client";
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { useForm } from 'react-hook-form';
 import { Switch } from '@/components/ui/switch';
 import { SearchIcon } from 'lucide-react';
+import axios from 'axios';
 
+interface User {
+	name: string,
+	cargo: string,
+  ativo: boolean
+}
 
 const ColaboradorPage = () => {
 	const form = useForm()
-	const [isChecked, setIsChecked] = useState(false);
+	const [users, setUsers] = useState<User[]>([])
+	const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState("")
+  const [showActive, setShowActive] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/buscartodosusuarios")
+      .then((response) => {
+        setUsers(response.data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err)
+        setLoading(false);
+      })
+  }, [])
+
+	const filteredUsers = users.filter(({ name, cargo, ativo }) =>
+    (showActive ? ativo : !ativo) &&
+    (name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    cargo.toLowerCase().includes(searchTerm.toLowerCase()))
+  )
 
 	return (
 		<div className="flex justify-center mt-20">
-			<Card className="w-4/12">
+			<Card className=" w-11/12 lg:w-5/12">
 				<CardHeader className="reducep-6">
 					<CardTitle className="text-center font-bold text-[32px] text-neutral leading-10 mb-0.5">
 						Usuários
@@ -32,46 +60,43 @@ const ColaboradorPage = () => {
 									<Input
 										className="rounded-6 text-base leading-5 border-neutral-300 pl-10"
 										placeholder="Pesquisar por nome ou CPF"
+										value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
 									/>
 								</div>
 							</FormControl>
 							<div>
-								<Switch className="bg-gray-200 checked:bg-green-400 relative inline-flex h-6 w-11 items-center rounded-full" />
+								<Switch checked={showActive}
+                  onCheckedChange={setShowActive} 
+                  className="bg-gray-200 checked:bg-green-400 relative inline-flex h-6 w-11 items-center rounded-full" />
+                <p>{showActive ? "Ativos" : "Inativos"}</p>
 							</div>
 						</div>
 					</Form>
 
 					<div className="py-5">
-						<div className="flex justify-between py-5">
-							<div className="flex gap-3">
-								<div className="w-14 h-14 bg-gray-400 rounded-full"></div>
-								<div>
-									<h4 className="font-semibold">André Souza</h4>
-									<p>Administrador</p>
-								</div>
-							</div>
-							<div>
-								<div className="w-6 h-6 bg-green-600 rounded-full"></div>
-								<p>Ativo</p>
-							</div>
-						</div>
-
-						<div className="flex justify-between py-5">
-							<div className="flex gap-3">
-								<div className="w-14 h-14 bg-gray-400 rounded-full"></div>
-								<div>
-									<h4 className="font-semibold">Paula Soares</h4>
-									<p>Usuário</p>
-								</div>
-							</div>
-							<div className="">
-								<div className="w-6 h-6 bg-gray-400 rounded-full"></div>
-								<p>Inativo</p>
-							</div>
-						</div>
-
-						<div></div>
-					</div>
+          {loading ? (
+              <p>Carregando...</p>
+            ) : filteredUsers.length > 0 ? (
+              filteredUsers.map(({ name, cargo, ativo }) => (
+                <div key={name} className="flex justify-between py-5">
+                  <div className="flex gap-3">
+                    <div className="w-14 h-14 bg-gray-400 rounded-full"></div>
+                    <div>
+                      <h4 className="font-semibold">{name}</h4>
+                      <p>{cargo}</p>
+                    </div>
+                  </div>
+                  <div className="">
+                    <div className="w-6 h-6 bg-green-400 rounded-full"></div>
+                    <p>{ativo ? "Ativo" : "Inativo"}</p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>Nenhum usuário encontrado.</p>
+            )}
+          </div>
 				</CardContent>
 			</Card>
 		</div>
