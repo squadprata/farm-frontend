31b5e2a4b18feb6fa2b19057f5ac53fbfcb6c27e
Douglas Trindade <douglastrindade22@hotmail.com>
2024-08-23 00:13:25 -0300

    chore: eslint-disable-next-line

--- src/app/(auth)/cadastro-do-colaborador/components/index.tsx
+++ src/app/(auth)/cadastro-do-colaborador/components/index.tsx
@@ -22,72 +22,92 @@ export const EmployeeRegistration = () => {
     setStep(step - 1);
   };
 
-  const handleSubmit = async(data: any) => {
+  const handleSubmit = async (data: any) => {
     const finalData = { ...formData, ...data };
-    try { 
+    try {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       const response = await usePost({
-          endpoint:'/cadastro',
-          body:{
+        endpoint: "/cadastro",
+        body: {
           name: finalData.username,
           cpf: finalData.cpf,
           crf: finalData.crf,
           email: finalData.email,
           cargo: finalData.role,
-          role: finalData.permissionType
-          },
-          headers:{
-            "Content-Type": "multipart/form-data"
-          }
-        }
-        )
+          role: finalData.permissionType,
+        },
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-      if (response.status === 201) setStep(step + 1)
-        
-    } catch (error:any) {
+      if (response.status === 201) setStep(step + 1);
+    } catch (error: any) {
       Swal.fire({
         icon: "error",
         title: error?.response?.data?.error || "Erro",
         timerProgressBar: true,
-        timer: 6000
-      })
+        timer: 6000,
+      });
     }
-    
   };
 
-  const handleResetToNewRegister = () =>{
-    setFormData({})
-    setStep(1)
+  const handleResetToNewRegister = () => {
+    setFormData({});
+    setStep(1);
   };
 
   return (
     <div className="flex flex-col w-full items-center">
-      {step != 4 &&
+      {step != 4 && (
         <div className="flex w-full max-w-3xl gap-6 pb-20">
-        <article className="flex flex-col w-1/3 items-center justify-center">
-          <p className={step === 1 ? ("text-neutral-400 font-semibold") : ("text-neutral-300 font-normal")}>
-            1. Login de usuarios
-          </p>
-          <Progress value={step === 1 ? 100 : 0} />
-        </article>
-        <article className="flex flex-col w-1/3 items-center justify-center">
-          <p className={step === 2 ? ("text-neutral-400 font-semibold") : ("text-neutral-300 font-normal")}>
-            2. Dados
-          </p>
-          <Progress value={step === 2 ? 100 : 0} />
-        </article>
-        <article className="flex flex-col w-1/3 items-center justify-center">
-          <p className={step === 3 ? ("text-neutral-400 font-semibold") : ("text-neutral-300 font-normal")}>
-            3. Permissões
-          </p>
-          <Progress value={step === 3 ? 100 : 0} />
-        </article>
-      </div>
-      }
+          <article className="flex flex-col w-1/3 items-center justify-center">
+            <p
+              className={
+                step === 1
+                  ? "text-neutral-400 font-semibold"
+                  : "text-neutral-300 font-normal"
+              }
+            >
+              1. Login de usuarios
+            </p>
+            <Progress value={step === 1 ? 100 : 0} />
+          </article>
+          <article className="flex flex-col w-1/3 items-center justify-center">
+            <p
+              className={
+                step === 2
+                  ? "text-neutral-400 font-semibold"
+                  : "text-neutral-300 font-normal"
+              }
+            >
+              2. Dados
+            </p>
+            <Progress value={step === 2 ? 100 : 0} />
+          </article>
+          <article className="flex flex-col w-1/3 items-center justify-center">
+            <p
+              className={
+                step === 3
+                  ? "text-neutral-400 font-semibold"
+                  : "text-neutral-300 font-normal"
+              }
+            >
+              3. Permissões
+            </p>
+            <Progress value={step === 3 ? 100 : 0} />
+          </article>
+        </div>
+      )}
 
       <div className="flex w-full justify-center">
         {step === 1 && <UserLogin onNext={handleNext} formData={formData} />}
         {step === 2 && (
-          <UserData onNext={handleNext} onPrevious={handlePrevious} formData={formData}/>
+          <UserData
+            onNext={handleNext}
+            onPrevious={handlePrevious}
+            formData={formData}
+          />
         )}
         {step === 3 && (
           <UserPermissions onNext={handleSubmit} onPrevious={handlePrevious} />
